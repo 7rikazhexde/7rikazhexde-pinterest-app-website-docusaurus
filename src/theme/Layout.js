2a139759f839026450667722d37771c76de88edc
7rikaz_h785 <7rikaz.h785.stat2ltas41lcijad@gmail.com>
2024-03-31 17:11:44 +0900

    feat: Added privacy polocy and cookie consent banner

---
+++ src/theme/Layout.js
@@ -0,0 +1,163 @@
+import React, { useEffect } from "react";
+import OriginalLayout from "@theme-original/Layout";
+import CookieConsent, { Cookies } from "react-cookie-consent";
+import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
+import { useLocation } from "@docusaurus/router";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+
+const containerStyle = {
+  background: "#bbdbf3",
+  color: "black",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+};
+
+const buttonStyle = {
+  borderRadius: "4px",
+  fontWeight: "bold",
+  cursor: "pointer",
+  backgroundColor: "#ffc107",
+  color: "black",
+  width: "120px",
+  height: "40px",
+  fontSize: "15px",
+};
+
+const allowButtonStyle = {
+  ...buttonStyle,
+  backgroundColor: "#2e8555",
+  color: "white",
+  marginRight: "30px",
+};
+
+const linkStyle = {
+  color: "blue",
+  textDecoration: "underline",
+};
+
+const denyButtonStyle = {
+  ...buttonStyle,
+};
+
+const githubTrackingId = "G-P182TEV17B"; // GitHub Pagesでデプロイしたサイト用のGA4 ID
+
+export default function Layout(props) {
+  const location = useLocation();
+  const { i18n, siteConfig } = useDocusaurusContext();
+  const { customFields } = siteConfig;
+  const isNetlifyProduction = customFields.deploy;
+  const currentLocale = i18n.currentLocale;
+
+  const trackingId = isNetlifyProduction ? netlifyTrackingId : githubTrackingId;
+
+  const localeTexts = {
+    ja: {
+      consentText: (
+        <span>
+          このサイトは、利便性を向上させるためにCookieを使用し、Google Analytics
+          (GA4)を活用しています。
+          <br />
+          Cookieの使用について、同意するか拒否するかを選択してください。
+          詳細については、
+          <a
+            href={
+              isNetlifyProduction
+                ? "https://7rikazhexde.github.io/7rikazhexde-pinterest-app-website-docusaurus/docs/privacy_policy"
+                : "https://7rikazhexde.github.io/7rikazhexde-pinterest-app-website-docusaurus/docs/privacy_policy"
+            }
+            style={linkStyle}
+          >
+            こちら
+          </a>
+          をご覧ください。
+        </span>
+      ),
+      buttonText: "同意する",
+      declineButtonText: "拒否する",
+    },
+    en: {
+      consentText: (
+        <span>
+          This site uses cookies and utilizes Google Analytics (GA4) to enhance
+          convenience.
+          <br />
+          Please select whether you agree or decline the use of cookies. For
+          more details, please see{" "}
+          <a
+            href={
+              isNetlifyProduction
+                ? "https://7rikazhexde.github.io/7rikazhexde-pinterest-app-website-docusaurus/en/docs/privacy_policy"
+                : "https://7rikazhexde.github.io/7rikazhexde-pinterest-app-website-docusaurus/en/docs/privacy_policy"
+            }
+            style={linkStyle}
+          >
+            here
+          </a>
+          .
+        </span>
+      ),
+      buttonText: "Agree",
+      declineButtonText: "Decline",
+    },
+  };
+
+  const texts = localeTexts[currentLocale] || localeTexts["en"];
+
+  const handleAcceptCookie = () => {
+    if (ExecutionEnvironment.canUseDOM) {
+      Cookies.set("docusaurus-gdpr-google-analytics", "true", { expires: 150 });
+      window.location.reload();
+    }
+  };
+
+  const handleDeclineCookie = () => {
+    if (ExecutionEnvironment.canUseDOM) {
+      Cookies.set("docusaurus-gdpr-google-analytics", "false", {
+        expires: 150,
+      });
+      window.location.reload();
+    }
+  };
+
+  useEffect(() => {
+    if (ExecutionEnvironment.canUseDOM) {
+      const cookieValue = Cookies.get("docusaurus-gdpr-google-analytics");
+      if (cookieValue === "true") {
+        const script = document.createElement("script");
+        script.src = `https://www.googletagmanager.com/gtag/js?id=${trackingId}`;
+        script.async = true;
+        document.head.appendChild(script);
+
+        window.dataLayer = window.dataLayer || [];
+        function gtag() {
+          window.dataLayer.push(arguments);
+        }
+        gtag("js", new Date());
+        gtag("config", trackingId, { anonymize_ip: true });
+        gtag("set", "page_path", location.pathname);
+        gtag("event", "page_view");
+      }
+    }
+  }, [location.pathname, trackingId]);
+
+  return (
+    <>
+      <OriginalLayout {...props} />
+      <CookieConsent
+        //debug={true}
+        location="bottom"
+        buttonText={texts.buttonText}
+        declineButtonText={texts.declineButtonText}
+        enableDeclineButton
+        style={containerStyle}
+        declineButtonStyle={denyButtonStyle}
+        buttonStyle={allowButtonStyle}
+        onAccept={handleAcceptCookie}
+        onDecline={handleDeclineCookie}
+      >
+        {texts.consentText}
+      </CookieConsent>
+    </>
+  );
+}
