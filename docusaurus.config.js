226f03bffa2a261c09c183a09bb194aa3701d680
7rikazhexde <33836132+7rikazhexde@users.noreply.github.com>
2024-03-31 15:41:07 +0900

    feat: Initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,173 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  //title: "7rikazhexde's Pinterest App Site",
+  title: "7rikazhexde's Pinterest App Web Site",
+  //tagline: 'Dinosaurs are cool',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://7rikazhexde.github.io',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/7rikazhexde-pinterest-app-website-docusaurus/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: '7rikazhexde', // Usually your GitHub org/user name.
+  projectName: '7rikazhexde-pinterest-app-website-docusaurus', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  //i18n: {
+  //  defaultLocale: 'ja',
+  //  locales: ['ja'],
+  //},
+
+  plugins: [
+    [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      {
+        indexDocs: true,
+        //language: ["en","ja"],
+      },
+    ],
+  ],
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: './sidebars.js',
+          // docs/index.mdをTopページにする場合にコメントアウトを外すこと。
+          routeBasePath: '/',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        //blog: {
+        //  showReadingTime: true,
+        //  // Please change this to your repo.
+        //  // Remove this to remove the "edit this page" links.
+        //  editUrl:
+        //    'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        //},
+        blog : false,
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'Home',
+        logo: {
+          alt: "7rikazhexde's Pinterest App Site Logo",
+          src: 'img/logo.svg',
+        },
+        items: [
+          //{
+          //  to: '/tutorial',
+          //  type: 'docSidebar',
+          //  sidebarId: 'tutorialSidebar',
+          //  position: 'left',
+          //  label: 'Tutorial',
+          //},
+          {
+            to: '/app',
+            type: 'docSidebar',
+            sidebarId: 'appSidebar',
+            position: 'left',
+            label: 'App',
+          },
+          //{to: '/blog', label: 'Blog', position: 'left'},
+          //{
+          //  href: 'https://github.com/facebook/docusaurus',
+          //  label: 'GitHub',
+          //  position: 'right',
+          //},
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              //{
+              //  label: 'Tutorial',
+              //  to: '/docs/tutorial/hogehoge',
+              //},
+              {
+                label: 'App',
+                //src/pages/index.jsをtopページにする場合
+                //to: '/docs/app/',
+                //docs/index.mdをTopページにする場合
+                //src/pages/index.jsをsrc/pages/index.js0にすること。
+                to: '/app',
+              },
+            ],
+          },
+          /*
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/docusaurus',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/docusaurus',
+              },
+            ],
+          },
+          */
+          {
+            title: 'More',
+            items: [
+              //{
+              //  label: 'Blog',
+              //  to: '/blog',
+              //},
+              //{
+              //  label: 'GitHub',
+              //  href: 'https://github.com/facebook/docusaurus',
+              //},
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} 7rikazhexde's Pinterest App.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
